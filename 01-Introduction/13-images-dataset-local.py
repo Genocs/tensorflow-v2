427f48b9ba8de2493b62ebf03e7a500cab8880bf
Giovanni Emanuele Nocco <giovanni.nocco@gmail.com>
2020-05-01 12:26:41 +0200

    loaded model and evaluate from local

--- 01-Introduction/13-images-dataset-local.py
+++ 01-Introduction/13-images-dataset-local.py
@@ -38,9 +38,8 @@ import datetime
 # Import python openCV
 import cv2
 
-# This function will plot images in the form of a grid with 1 row and 5 columns where images are placed in each column.
 
-
+# Plot images in the form of a grid with 1 row and 5 columns where images are placed in each column.
 def plotImages(images_arr):
     fig, axes = plt.subplots(1, 5, figsize=(20, 20))
     axes = axes.flatten()
@@ -60,11 +59,12 @@ def build_model():
     # Define the  model type
     model = tf.keras.Sequential()
 
-    # Define the graph 
-    model.add(layers.Conv2D(32, (3, 3),
-                activation='relu',
-                input_shape=(IMG_WIDTH, IMG_HEIGHT, CHANNELS),
-                data_format="channels_last"))
+    # Define the graph
+    model.add(
+        layers.Conv2D(32, (3, 3),
+                      activation='relu',
+                      input_shape=(IMG_WIDTH, IMG_HEIGHT, CHANNELS),
+                      data_format="channels_last"))
 
     model.add(layers.MaxPooling2D((2, 2)))
     model.add(layers.Conv2D(64, (3, 3), activation='relu'))
@@ -74,14 +74,16 @@ def build_model():
     model.add(layers.Dense(64, activation='relu'))
     model.add(layers.Dense(5))
 
-    model.compile(optimizer='adam',
-                    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-                    metrics=['accuracy'])
+    model.compile(
+        optimizer='adam',
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=['accuracy'])
 
     # print the model structure
     model.summary()
 
     return model
+
 
 def run_training(model, samples, labels):
     log_path = 'c:\\log\\'
@@ -98,69 +100,52 @@ def run_training(model, samples, labels):
                         callbacks=[tensorboard_callback])
     model.summary()
 
-    return model 
-
-
-def instruct_model(model, samples, labels):
-    model_dir = ".\\model\\"
-
-    if os.path.isdir(model_dir):
-        print('Found model.')
-        print('Loading weights ...')
-        print('NO Training required')
-        model.load_weights(model_dir + "model.tf")
-    else:
-        print('Model do not exit!')
-        print('A new one training is required!')
-        model = run_training(model, samples, labels)
-        print('Model weights SAVING... ')
-        model.save_weights(model_dir + "model.tf")
-        print('Model weights SAVED SUCCESSFULLY')
-
     return model
 
 
+# Check if exists the model weights, based on the directory where the weights are stored
+def exist_weights():
+    model_dir = ".\\model\\"
 
-def scan_local(data_path, log_path='c:\\log\\'):
-    writer = tf.summary.create_file_writer(log_path)
+    return os.path.isdir(model_dir)
 
-    with writer.as_default():
-        tf.summary.scalar("my_metric", 0.5, step=22)
-        writer.flush()
 
+# Load the trainingset from local filesystem
+def get_traingset(data_path):
     # Load image from local file system
     data_dir = pathlib.Path(data_path)
-
     data_dir = pathlib.Path(data_dir)
     image_count = len(list(data_dir.glob('*/*.jpg')))
 
-    print('image_count: %d' % image_count)
+    # Simple log
     print('data_dir: %s' % data_dir)
+    print('image_count: %d' % image_count)
 
-    CLASS_NAMES = np.array([
-        item.name for item in data_dir.glob('*') if item.name != "LICENSE.txt"
-    ])
+    # Get the class name based on the trainingset folder
+    CLASS_NAMES = np.array([item.name for item in data_dir.glob('*')])
+
+    # Print the class names
     print('CLASS_NAMES: %s' % CLASS_NAMES)
 
+    BATCH_SIZE = 256
+    IMG_HEIGHT = 64
+    IMG_WIDTH = 64
+
+    # Prepare the image dataset generator
     # The 1./255 is to convert from uint8 to float32 in range [0,1].
     image_generator = tf.keras.preprocessing.image.ImageDataGenerator(
         rescale=1. / 255)
 
-    BATCH_SIZE = 256
-    CHANNELS = 3
-    IMG_HEIGHT = 64
-    IMG_WIDTH = 64
-    STEPS_PER_EPOCH = np.ceil(image_count / BATCH_SIZE)
-
     train_data_gen = image_generator.flow_from_directory(
-                                                        directory=str(data_dir),
-                                                        batch_size=BATCH_SIZE,
-                                                        shuffle=True,
-                                                        target_size=(IMG_HEIGHT, IMG_WIDTH),
-                                                        classes=list(CLASS_NAMES))
+        directory=str(data_dir),
+        batch_size=BATCH_SIZE,
+        shuffle=True,
+        target_size=(IMG_HEIGHT, IMG_WIDTH),
+        classes=list(CLASS_NAMES))
 
     train_images, train_labels = next(train_data_gen)
 
+    # remoge comment to see some TS images
     #plotImages(train_images[:5])
 
     # Convert the oneshot label vector to classes vectors
@@ -173,39 +158,78 @@ def scan_local(data_path, log_path='c:\\log\\'):
 
     training_label_classes = np.asarray(vector_classes_list, dtype=np.float32)
 
+    print('CLASS_NAMES:')
+    print(training_label_classes)
+
+    return train_images, training_label_classes, CLASS_NAMES
+
+# Load the image to inspect from local filesystem
+def get_images(data_path):
+    # Load image from local file system
+    data_dir = pathlib.Path(data_path)
+    data_dir = pathlib.Path(data_dir)
+
+    BATCH_SIZE = 256
+    IMG_HEIGHT = 64
+    IMG_WIDTH = 64
+
+    # Prepare the image dataset generator
+    # The 1./255 is to convert from uint8 to float32 in range [0,1].
+    image_generator = tf.keras.preprocessing.image.ImageDataGenerator(
+        rescale=1. / 255)
+
+    train_data_gen = image_generator.flow_from_directory(
+        directory=str(data_dir),
+        batch_size=BATCH_SIZE,
+        shuffle=True,
+        target_size=(IMG_HEIGHT, IMG_WIDTH))
+
+    images, labels = next(train_data_gen)
+
+    return images, labels
+
+
+def scan_local(data_path, log_path='c:\\log\\'):
+    model_weights_path = ".\\model\\weights.tf"
+
+    writer = tf.summary.create_file_writer(log_path)
+
+    with writer.as_default():
+        tf.summary.scalar("my_metric", 0.5, step=22)
+        writer.flush()
+
+    # Build the model graph
     model = build_model()
-    model = instruct_model(model, train_images, training_label_classes)
+
+    # Check if the weights exist and if dont than go to training the model and save it
+    if (exist_weights()):
+        print('Found model weights, NO Training required. Loading...')
+        model.load_weights(model_weights_path)
+        print('Model weights LOADED SUCCESSFULLY...  go straight to evaluate!!')
+    else:
+        train_images, _labels, _CLASS_NAMES = get_traingset(data_path)
+        model = run_training(model, train_images, _labels)
+        print('Model weights SAVING... ')
+        model.save_weights(model_weights_path)
+        print('Model weights SAVED SUCCESSFULLY')
 
     # Evaluate a sample using the model
-    predictions = model.predict(train_images[0:2])
+    evaluate_images, labels = get_images('E:/Data/UTU/evaluate')
+    predictions = model.predict(evaluate_images)
 
+
+    CLASS_NAMES = np.array(['globalblue_f', 'globalblue_m', 'globalblue_t', 'others', 'planet'])
+    print(predictions)
     print('predict[0]: %s' % CLASS_NAMES[np.argmax(predictions[0])])
     print('predict[1]: %s' % CLASS_NAMES[np.argmax(predictions[1])])
-    print('labels: %s' % training_label_classes[0:2])
+    print('predict[2]: %s' % CLASS_NAMES[np.argmax(predictions[2])])
 
-    return model
+    plotImages(evaluate_images)
 
 
 def main():
-    #weightfile = "weights/utu_direct.weights"
-    #cfgfile = "cfg/utu_direct.cfg"
-    #model_size = (416, 416, 3)
-    #num_classes = 5
-    model = scan_local(data_path='E:/Data/UTU/directinvoice_img')
-    # evaluate model
-    #print('predict: %s' % model.predict(train_images[0:2]))
-    #print('labels: %s' % training_label_classes[0:2])
+    scan_local(data_path='E:/Data/UTU/directinvoice_img')
 
-    #img_dir = 'E:\\Data\\UTU\\directinvoice_img\\globalblue_f\\12083.027006.5344.44365.jpg'
-    #image = cv2.imread(img_dir)
-    #image = np.array(image)
-    #model.predict(image)
-    #plt.figure()
-    #plt.imshow(image)
-    #plt.colorbar()
-    #plt.grid(False)
-    #plt.grid(False)
-    #plt.show()
 
 if __name__ == '__main__':
     main()
